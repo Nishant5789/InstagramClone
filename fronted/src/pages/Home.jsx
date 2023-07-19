4de46ad85fa30ab91b3ac5d93a5461864cfe8b2a
Nishant5789 <nishantbhandari218@gmail.com>
2023-07-19 10:14:13 +0530

    feat : Intialize with create-react-app

---
+++ fronted/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../feature/Navbar/components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Home
