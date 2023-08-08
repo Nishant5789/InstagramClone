44235a8d8207ecb8e358875d3c622e289dc7b036
Nishant5789 <nishantbhandari218@gmail.com>
2023-08-08 21:58:09 +0530

    feat : added jsx for chatpage

--- fronted/src/pages/Chat.js
+++ fronted/src/pages/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ChatNavbar from '../feature/Chat/components/ChatNavbar'
 import { Chatlist } from '../feature/Chat/components/Chatlist'
+import Chatpanel from '../feature/Chat/components/Chatpanel'
 
 
 const Chat = () => {
@@ -8,7 +9,7 @@ const Chat = () => {
     <div className='grid grid-cols-12'>
      <ChatNavbar/>
      <Chatlist/>
-    <div className='border-2 h-screen border-purple-500 col-span-10 md:col-span-9 lg:col-span-7'></div>
+     <Chatpanel/>
   </div>
   )
 }
