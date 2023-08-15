4f997225fa22805972214577b4550f1a9b61b6af
Nishant5789 <nishantbhandari218@gmail.com>
2023-08-15 16:55:03 +0530

    feat : added profile component

--- fronted/src/pages/Home.js
+++ fronted/src/pages/Home.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import Status from '../feature/Status/components/Status'
 import Posts from '../feature/Post/components/Posts'
 import Notificationpanel from '../feature/Notification/components/Notificationpanel'
 import FollowRequest from '../feature/Notification/components/FollowRequest'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, json, useParams } from 'react-router-dom'
+import Profile from '../feature/Profile/components/Profile'
 
 
 
 const Home = () => {
+  const [isProfile, setIsProfile] = useState(false);
+  // const [isStatus, setIsStatus] = useState(false);
+  const RenderPath = useParams()["*"];
+
+  useEffect(()=>{
+    console.log("effect");
+    if(RenderPath === 'Profile')
+      setIsProfile(true);
+    else
+      setIsProfile(false);
+  },[RenderPath])
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-12 relative'>
       <Navbar />
-      <div className='lg:col-span-6 col-span-11  border-2'>
-        <Status />
+      <div className={`${isProfile ? "lg:col-span-10" : "lg:col-span-6"} col-span-11  border-2`}>
+        {/* <Status /> */}
         <Routes>
           <Route>
             <Route index element={<Posts />} />
             <Route path="/Notification" element={<Notificationpanel />} />
             <Route path="/Followrequest" element={<FollowRequest />} />
+            <Route path="/Profile" element={<Profile />} />
           </Route>
         </Routes>
       </div>
