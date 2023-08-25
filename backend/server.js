af4382ffe8d83de76342566d28890ec7e631400b
Devanshu Chhipani <devanshuchhipani@gmail.com>
2023-08-25 23:27:00 +0530

    feat : Adding All modeles of database.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/user",require("./routes/userRoutes"));
+//app.use("/api/user",require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
